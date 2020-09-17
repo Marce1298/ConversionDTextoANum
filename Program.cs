11935fba8aab8252f12ec6b48f460bbc469454c0
Marce1298 <marchebordon@gmail.com>
2020-09-17 19:18:18 -0300

    ConversionDTextoANum

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Dynamic;
+
+namespace ConversionDTextoANum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce el primer numero");
+
+            int num1=int.Parse (Console.ReadLine());
+
+            Console.WriteLine("Introduce el segundo numero");
+
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"El resultado es {num1 + num2}");
+
+        }
+    }
+}
